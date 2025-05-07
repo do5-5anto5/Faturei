c04dbd4ea58e4d318cd7f0fe599dbf355c614626
Jonathan <dos.santos.2k23@gmail.com>
2025-05-07 19:41:03 -0300

    implement SplashScreen and AnimatedSplashLogo

---
+++ lib/pages/splash.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../widgets/animated_logo.dart';
+
+// Responsibilities: time control and navigation to home
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await Future.delayed(const Duration(seconds: 3), () {
+        if (mounted) {
+          Get.offNamed('/home');
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(body: Center(child: Container(
+      child: AnimatedSplashLogo()
+    ))));
+  }
+}
