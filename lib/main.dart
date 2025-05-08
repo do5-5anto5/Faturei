2550c1475ea2c0d7c539612d9acb954f2a1974d6
Jonathan <dos.santos.2k23@gmail.com>
2025-05-08 19:14:12 -0300

    change color seed

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
         useMaterial3: true
       ),
       initialRoute: '/splash',
