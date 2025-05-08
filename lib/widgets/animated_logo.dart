60c0fc8593dab4cf09e36ab32bf08deaf4d2b829
Jonathan <dos.santos.2k23@gmail.com>
2025-05-08 18:47:09 -0300

    adjust AnimatedSplashLogo

--- lib/widgets/animated_logo.dart
+++ lib/widgets/animated_logo.dart
@@ -37,40 +37,53 @@ class _AnimatedSplashLogoState extends State<AnimatedSplashLogo>
     });
 
     _animation = Tween<double>(begin: 50.0, end: 150.0).animate(
-        CurvedAnimation(parent: _animationController!, curve: Curves.easeInOut));
+      CurvedAnimation(parent: _animationController!, curve: Curves.easeInOut),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: AnimatedBuilder(
-          animation: _animation!,
-          child: Image.asset('assets/icons/logo.png'),
-          builder: (context, child) {
-            // that will pulse on screen
-            return Container(
-              width: _animation!.value,
-              height: _animation!.value,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Colors.grey,
-                    Colors.grey.withValues(alpha: 0.5),
-                  ],
-                  begin: Alignment.centerLeft,
-                  end: Alignment.centerRight,
-                ),
-                // adds blur to gradient
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: Colors.transparent,
-                  width: 4,
-                ),
-                boxShadow: [],
+        animation: _animation!,
+        child: Image.asset('assets/icons/logo.png'),
+        builder: (context, child) {
+          // that will pulse on screen
+          return Container(
+            width: _animation!.value,
+            height: _animation!.value,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.grey, Colors.grey.withValues(alpha: 0.05)],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
               ),
-              child: child,
-            );
-          }),
+              // adds blur to gradient
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: Colors.transparent,
+                width: 4,
+              ),
+              boxShadow:
+                  !hasBlur
+                      ? [
+                        BoxShadow(
+                          color: Colors.grey,
+                          spreadRadius: 18,
+                          blurRadius: 9,
+                        ),
+                        BoxShadow(
+                          color: Colors.grey.withValues(alpha: 0.5),
+                          spreadRadius: 18,
+                          blurRadius: 9,
+                        ),
+                      ]
+                      : [],
+            ),
+            child: child,
+          );
+        },
+      ),
     );
   }
 }
