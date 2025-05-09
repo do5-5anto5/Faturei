d95989f6e6b2657a231573fcb61745982a36a6f5
Jonathan <dos.santos.2k23@gmail.com>
2025-05-09 19:40:06 -0300

    Implement AuthPage and add it's route to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
+import 'package:faturei/pages/auth.dart';
 import 'package:faturei/pages/homepage.dart';
 import 'package:faturei/pages/second.dart';
 import 'package:faturei/pages/splash.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'binds/main.dart';
+import 'firebase_options.dart';
 
-void main() {
+late final FirebaseApp app;
+late final FirebaseAuth auth;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  app = await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  auth = FirebaseAuth.instanceFor(app: app);
+
   runApp(const MyApp());
 }
 
@@ -26,6 +40,10 @@ class MyApp extends StatelessWidget {
         GetPage(
             name: '/splash',
             page: () => const SplashScreen()
+        ),
+        GetPage(
+            name: '/auth',
+            page: () => const AuthPage()
         ),
         GetPage(
           name: '/home',
