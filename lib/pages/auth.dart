f4df8e19c013dec30d7adbedce296b9a44005c43
Jonathan <dos.santos.2k23@gmail.com>
2025-05-10 15:11:01 -0300

    Personalize SignInScreen strings

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
@@ -12,9 +13,54 @@ class AuthPage extends StatelessWidget {
       providers: providers,
       actions: [
         AuthStateChangeAction<UserCreated>((context, state) {
-          Navigator.pushReplacementNamed(context, '/profile');
+          Get.offNamed('/home');
+        }),
+        AuthStateChangeAction<SignedIn>((context, state) {
+          Get.offNamed('/home');
         }),
       ],
+      headerBuilder: (context, constraints, shrinkOffset) {
+        return Image.asset(
+          'assets/icons/logo.png',
+          height: constraints.maxHeight,
+          width: constraints.maxWidth,
+        );
+      },
+      sideBuilder: (context, constraints) {
+        return Image.asset(
+          'assets/icons/logo.png',
+          height: constraints.maxHeight,
+          width: constraints.maxWidth,
+        );
+      },
+      subtitleBuilder: (context, action) {
+        final actionText = switch (action) {
+          AuthAction.signIn => 'Por favor, insira as credenciais.',
+          AuthAction.signUp => 'Por favor, cadastre seu email e senha.',
+        _ => throw Exception('Invalid action: $action'),
+        };
+        return Padding(
+        padding: const EdgeInsets.only(bottom: 8),
+        child: Text('Bem vindo ao Faturei! $actionText'),
+        );
+      },
+      footerBuilder: (context, action) {
+        final actionText = switch (action) {
+          AuthAction.signIn => 'Entrar',
+          AuthAction.signUp => 'Inscrever-se',
+          _ => throw Exception('Invalid action: $action'),
+        };
+
+        return Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 16),
+            child: Text(
+              'Após $actionText, você aceita os termos e condições.',
+              style: const TextStyle(color: Colors.grey),
+            ),
+          ),
+        );
+      },
     );
   }
 }
